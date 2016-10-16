f325d03a914a20850e9779592475a33afd06b3bd
Marta Klopf <mklopf@martas-mbp.usinternet.com>
2016-10-16 10:10:57 -0500

    random language transator

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// var languages = ["af", "ga", "sq", "it", "ar", "ja", "az", "kn", "eu", "ko", "bn", "la", "be", "lv", "bg", "lt", "ca", "mk", "zh-CN", "ms", "zh-TW", "mt", "hr", "no", "cs", "fa", "da", "pl", "nl", "pt", "en", "ro", "eo", "ru", "et", "sr", "tl", "sk", "fi", "sl", "fr", "es", "gl", "sw", "ka", "sv", "de", "ta", "el", "te", "gu", "th", "ht", "tr", "iw", "uk", "hi", "ur", "hu", "vi", "is", "cy", "id", "yi"];
-// var tar = languages[Math.floor(languages.length * Math.random())]
+var languages = ["af", "ga", "sq", "it", "ar", "ja", "az", "kn", "eu", "ko", "bn", "la", "be", "lv", "bg", "lt", "ca", "mk", "zh-CN", "ms", "zh-TW", "mt", "hr", "no", "cs", "fa", "da", "pl", "nl", "pt", "en", "ro", "eo", "ru", "et", "sr", "tl", "sk", "fi", "sl", "fr", "es", "gl", "sw", "ka", "sv", "de", "ta", "el", "te", "gu", "th", "ht", "tr", "iw", "uk", "hi", "ur", "hu", "vi", "is", "cy", "id", "yi"];
+var tar = languages[Math.floor(languages.length * Math.random())]
+
 
 
 
@@ -35,7 +36,7 @@ console.log(socket.id)
   socket.on('addWords', function(data){ //look for any massages with addWords
     console.log('addWords' + data.content); //log out the data
 
-    var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161010T221631Z.cb30aad71732ef99.335507e44d898a9e7adb84299fb50fe1e7d2481d&text='+ data.content +'&lang=en-de'
+    var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161010T221631Z.cb30aad71732ef99.335507e44d898a9e7adb84299fb50fe1e7d2481d&text='+ data.content +'&lang=en-'+ tar +''
     request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) { //if there is no errors or it does not exist
           var parsedBody = JSON.parse(body)
